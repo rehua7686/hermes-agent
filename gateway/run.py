samlin0320-bee/https-github.com-NousResearch-hermes-agent

feat(gateway): Nextcloud Notifications + Activity background service
Introduces a Services abstraction for non-chat background event listeners
in the gateway, alongside the existing Platforms (chat channels).

Polls both NC Notifications API (ETag-optimized) and Activity API to catch
all Nextcloud events. Rules engine classifies events as react (trigger agent
session) or silent (persistent storage for later retrieval).

Architecture:
- BaseService ABC with start/stop lifecycle, separate from BasePlatformAdapter
- GatewayRunner integration: services start after platforms, stop before
- Credential sharing from Nextcloud Talk platform config

Event processing:
- NotificationClient with ETag-optimized polling
- Activity API client-side filtering (NC since= paginates backwards)
- Sender extraction from subjectRichParameters/subject_rich
- Self-generated events filtered to prevent loops
- Rules engine matches on app, object_type, AND type fields

React delivery:
- Injects MessageEvent into target platform adapter via handle_message()
- Uses adapter _classify_chat() for correct session key matching
- Full agent session with conversation context preserved

Silent storage:
- Persistent JSON storage (~/.hermes/notification_events.json)
- Query by app and timestamp, FIFO with configurable max

Supported react events (Activity API field values):
- deck:deck_card — card assigned to agent
- deck:deck_card (type=deck_card_description) — silenced
- files_sharing:files (type=shared) — file shared with agent
- files:files (type=file_created) — new file in shared folder
- dav:calendar_event — calendar invitation

File handling (download, upload, sync) is intentionally NOT part of this
service. A separate NextcloudFilesService will handle file operations
with proper bidirectional sync via WebDAV + notify_push WebSocket.

Includes user guide documentation, cli-config.yaml.example, 26 tests.

gateway/run.py
@@ -617,6 +617,7 @@ class GatewayRunner:
         self.config = config or load_gateway_config()
         self.adapters: Dict[Platform, BasePlatformAdapter] = {}
         self._warn_if_docker_media_delivery_is_risky()
+        self.services: dict = {}  # Background services (non-chat event listeners)
 
         # Load ephemeral config from config.yaml / env vars.
         # Both are injected at API-call time only and never persisted.
@@ -2166,6 +2167,22 @@ class GatewayRunner:
             )
         asyncio.create_task(self._platform_reconnect_watcher())
 
+        # Start background services
+        for svc_name, svc_config in self.config.services.items():
+            if not isinstance(svc_config, dict) or not svc_config.get("enabled", False):
+                continue
+            svc = self._create_service(svc_name, svc_config)
+            if svc:
+                svc.gateway_runner = self
+                try:
+                    if await svc.start():
+                        self.services[svc_name] = svc
+                        logger.info("Service %s started", svc_name)
+                    else:
+                        logger.warning("Service %s failed to start", svc_name)
+                except Exception as e:
+                    logger.error("Service %s startup error: %s", svc_name, e)
+
         logger.info("Press Ctrl+C to stop")
         
         return True
@@ -2594,6 +2611,16 @@ class GatewayRunner:
                 except Exception as _e:
                     logger.debug("SessionDB close error: %s", _e)
 
+# Stop services first (before platforms, since services may be mid-delivery)
+        for svc_name, svc in list(self.services.items()):
+            try:
+                await svc.stop()
+                logger.info("Service %s stopped", svc_name)
+            except Exception as e:
+                logger.error("Service %s stop error: %s", svc_name, e)
+        self.services.clear()
+
+
             from gateway.status import remove_pid_file
             remove_pid_file()
 
@@ -2794,6 +2821,22 @@ class GatewayRunner:
                 return None
             return QQAdapter(config)
 
+        return None
+
+    def _create_service(self, name: str, config: dict):
+        """Factory for background services."""
+        if name == "nextcloud_notifications":
+            from gateway.services.nextcloud_notifications import NextcloudNotificationService
+            svc_config = dict(config.get("extra", config))
+            # Inherit NC credentials from Talk platform if not set
+            if not svc_config.get("nextcloud_url"):
+                talk_cfg = self.config.platforms.get(Platform.NEXTCLOUD_TALK) if hasattr(Platform, "NEXTCLOUD_TALK") else None
+                if talk_cfg and talk_cfg.extra:
+                    svc_config.setdefault("nextcloud_url", talk_cfg.extra.get("nextcloud_url", ""))
+                    svc_config.setdefault("username", talk_cfg.extra.get("username", "hermes"))
+                    svc_config.setdefault("app_password_env", talk_cfg.extra.get("app_password_env", "NEXTCLOUD_TALK_APP_PASSWORD"))
+            return NextcloudNotificationService(svc_config)
+        logger.warning("Unknown service: %s", name)
         return None
 
     def _is_user_authorized(self, source: SessionSource) -> bool:

rehua7686/hermes-agent

gateway/run.py
@@ -1840,6 +1840,7 @@ class GatewayRunner:
         self.adapters: Dict[Platform, BasePlatformAdapter] = {}
         self._warn_if_docker_media_delivery_is_risky()
         _gateway_runner_ref = _weakref.ref(self)
+        self.services: dict = {}  # Background services (non-chat event listeners)
 
         # Load ephemeral config from config.yaml / env vars.
         # Both are injected at API-call time only and never persisted.
@@ -4782,6 +4783,22 @@ class GatewayRunner:
         # turn so the agent kicks off the new chat.
         asyncio.create_task(self._handoff_watcher())
 
+        # Start background services
+        for svc_name, svc_config in self.config.services.items():
+            if not isinstance(svc_config, dict) or not svc_config.get("enabled", False):
+                continue
+            svc = self._create_service(svc_name, svc_config)
+            if svc:
+                svc.gateway_runner = self
+                try:
+                    if await svc.start():
+                        self.services[svc_name] = svc
+                        logger.info("Service %s started", svc_name)
+                    else:
+                        logger.warning("Service %s failed to start", svc_name)
+                except Exception as e:
+                    logger.error("Service %s startup error: %s", svc_name, e)
+
         logger.info("Press Ctrl+C to stop")
         
         return True
@@ -6681,6 +6698,15 @@ class GatewayRunner:
                 _phase_elapsed(),
             )
 
+            # Stop services before tearing down the rest (services may be mid-delivery)
+            for _svc_name, _svc in list(self.services.items()):
+                try:
+                    await _svc.stop()
+                    logger.info("Service %s stopped", _svc_name)
+                except Exception as _e:
+                    logger.error("Service %s stop error: %s", _svc_name, _e)
+            self.services.clear()
+
             from gateway.status import remove_pid_file, release_gateway_runtime_lock
             remove_pid_file()
             release_gateway_runtime_lock()
@@ -7018,6 +7044,22 @@ class GatewayRunner:
             if isinstance(extra, dict):
                 policy = extra.get("dm_policy")
         return str(policy or "").strip().lower()
+
+    def _create_service(self, name: str, config: dict):
+        """Factory for background services."""
+        if name == "nextcloud_notifications":
+            from gateway.services.nextcloud_notifications import NextcloudNotificationService
+            svc_config = dict(config.get("extra", config))
+            # Inherit NC credentials from Talk platform if not set
+            if not svc_config.get("nextcloud_url"):
+                talk_cfg = self.config.platforms.get(Platform.NEXTCLOUD_TALK) if hasattr(Platform, "NEXTCLOUD_TALK") else None
+                if talk_cfg and talk_cfg.extra:
+                    svc_config.setdefault("nextcloud_url", talk_cfg.extra.get("nextcloud_url", ""))
+                    svc_config.setdefault("username", talk_cfg.extra.get("username", "hermes"))
+                    svc_config.setdefault("app_password_env", talk_cfg.extra.get("app_password_env", "NEXTCLOUD_TALK_APP_PASSWORD"))
+            return NextcloudNotificationService(svc_config)
+        logger.warning("Unknown service: %s", name)
+        return None
 
     def _is_user_authorized(self, source: SessionSource) -> bool:
         """